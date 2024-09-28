f7805ce79e7046f66b2ddac636b72846780434c5
Mudit Rajput <muditrajputpersonal@gmail.com>
2024-09-28 12:15:14 +0530

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
-import Footer from './components/Footer/page';
+import Script from "next/script";
+import Footer from "./components/Footer/page";
 import Header from "./components/Header/page";
 import "./globals.css";
 const geistSans = localFont({
@@ -15,19 +16,38 @@ const geistMono = localFont({
 
 export const metadata = {
   title: "AI Text to Human Text Converter",
-  description: "Unlock the power of AI with our innovative Ai Text to Human Text Converter, designed to transform robotic AI-generated content into engaging, human-like writing. Enhance readability and boost productivity with our easy-to-use tool ai to human text. Ideal for content creators, marketers, and anyone seeking to elevate their written communication. Discover the future of content generation today!"
+  description:
+    "Unlock the power of AI with our innovative Ai Text to Human Text Converter, designed to transform robotic AI-generated content into engaging, human-like writing. Enhance readability and boost productivity with our easy-to-use tool ai to human text. Ideal for content creators, marketers, and anyone seeking to elevate their written communication. Discover the future of content generation today!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="google-site-verification" content="NxecJ-4yVr49rQWpDEUazLB2qEgg9srS1fH95mJEEK0" />
+      <meta
+        name="google-site-verification"
+        content="NxecJ-4yVr49rQWpDEUazLB2qEgg9srS1fH95mJEEK0"
+      />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZQPPS7D03S"
+      ></script>
+      <Script 
+        dangerouslySetInnerHTML={{
+          __html : ` window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-ZQPPS7D03S');
+        `,
+        }}
+      />
+       
+     
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
